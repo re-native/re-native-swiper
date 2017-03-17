9c0e9c0d3b174216e12e64c51a790f50d696dd17
Fabian Aussems <fabianaussems@gmail.com>
2017-03-17 18:26:34 +0100

    move re-native to re_native

--- project.clj
+++ project.clj
@@ -1,1 +1,6 @@
-(defproject re-native/swiper "1.5.4")
+(defproject re-native/swiper "1.5.4"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
+                 [org.clojure/clojurescript "1.9.473"]
+                 [reagent "0.6.0" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server]]
+                 [re-frame "0.9.1"]]
+  :plugins [[lein-cljsbuild "1.1.4"]])
